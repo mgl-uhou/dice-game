9f7d431fda004be1aa62520aad4c3f4c3c079466
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-07-21 11:55:20 -0300

    feat: do the dice compose

--- app/src/main/java/com/example/dicegame/MainActivity.kt
+++ app/src/main/java/com/example/dicegame/MainActivity.kt
@@ -23,10 +23,12 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.foundation.Canvas
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.dicegame.ui.theme.DiceGameTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,8 +49,98 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun DrawScope.circle(offset: () -> Offset){
+    val radius = Dp(20f).value
+    drawCircle(
+        Color.Black,
+        radius = radius,
+        center = offset()
+    )
+}
+
+fun DrawScope.center(){
+    circle {
+        Offset(size.width / 2, size.height / 2)
+    }
+}
+
+fun DrawScope.centerRight(){
+    circle {
+        Offset(size.width - Dp(20f).value, size.height / 2)
+    }
+}
+
+fun DrawScope.centerLeft(){
+    circle {
+        Offset(Dp(40f).value, size.height / 2)
+    }
+}
+
+fun DrawScope.topRight(){
+    circle {
+        Offset(size.width - Dp(20f).value, Dp(40f).value)
+    }
+}
+
+fun DrawScope.topLeft(){
+    circle {
+        Offset(Dp(40f).value, Dp(40f).value)
+    }
+}
+
+fun DrawScope.bottomRight(){
+    circle {
+        Offset(size.width - Dp(20f).value, size.height - Dp(20f).value)
+    }
+}
+
+fun DrawScope.bottomLeft() {
+    circle {
+        Offset(Dp(40f).value, size.height - Dp(20f).value)
+    }
+}
+
+fun DrawScope.bullet(number: Int) {
+    when(number){
+        1 -> {
+            center()
+        }
+        2 -> {
+            topRight()
+            bottomLeft()
+        }
+        3 -> {
+            topRight()
+            center()
+            bottomLeft()
+        }
+        4 -> {
+            topLeft()
+            topRight()
+            bottomLeft()
+            bottomRight()
+        }
+        5 -> {
+            topLeft()
+            topRight()
+            center()
+            bottomLeft()
+            bottomRight()
+        }
+        6 -> {
+            topLeft()
+            topRight()
+            centerRight()
+            centerLeft()
+            bottomLeft()
+            bottomRight()
+        }
+        else -> {}
+    }
+}
+
 @Composable
-fun Dice(modifier: Modifier){
+fun Dice(number: Int, modifier: Modifier){
     Canvas(
         modifier = modifier
             .size(96.dp, 96.dp)
@@ -59,12 +151,7 @@ fun Dice(modifier: Modifier){
             topLeft = Offset(10f, 10f),
             size = size
         )
-
-        drawCircle(
-            Color.Black,
-            radius = Dp(20f).value,
-            center = Offset(size.width / 2, size.height / 2)
-        )
+       bullet(number = number)
     }
 }
 
@@ -74,7 +161,7 @@ fun App() {
         .fillMaxSize()
         .background((Color.Black))
     ) {
-        Dice(modifier = Modifier.align(Alignment.Center))
+        Dice(number = 1, modifier = Modifier.align(Alignment.Center))
 
        Button(
            onClick = {  },
