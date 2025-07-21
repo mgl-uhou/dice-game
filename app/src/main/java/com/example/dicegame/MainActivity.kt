d3505041875e7aacc058f95ca20dfee9496d5862
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-07-21 10:35:27 -0300

    feature: create and align the dice and the button

--- app/src/main/java/com/example/dicegame/MainActivity.kt
+++ app/src/main/java/com/example/dicegame/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.foundation.Canvas
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.dicegame.ui.theme.DiceGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +34,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DiceGameTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold { padding ->
+                    Surface(
+                        modifier = Modifier.fillMaxSize().padding(padding),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        App()
+                    }
                 }
             }
         }
@@ -31,17 +48,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background((Color.Black))
+    ) {
+        Canvas(modifier = Modifier.size(96.dp, 96.dp).align(Alignment.Center)){
+            drawRoundRect(
+                Color.Green,
+                cornerRadius = CornerRadius(20f, 20f),
+                topLeft = Offset(10f, 10f),
+                size = size
+            )
+
+            drawCircle(
+                Color.Black,
+                radius = Dp(20f).value,
+                center = Offset(size.width / 2, size.height / 2)
+            )
+        }
+
+       Button(
+           onClick = {  },
+           modifier = Modifier
+               .align(Alignment.Center)
+               .offset(y = (100).dp)
+           ) {
+           Text("Jogar")
+       }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DiceGameTheme {
-        Greeting("Android")
+        Surface(modifier = Modifier.fillMaxSize()){
+            App()
+        }
     }
 }
